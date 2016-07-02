0d6bd618c412b9691199e185d050b5d35b72dcf2
Baoyang Li <lbyang222@gmail.com>
2016-07-02 16:10:24 +0800

    add file via upload (react-router-1)

---
+++ react-router-1/app/components/Home.js
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var Home = React.createClass({
+
+  render: function() {
+    return (
+      <div> Hello frome Home </div>
+    );
+  }
+});
+
+module.exports = Home;
