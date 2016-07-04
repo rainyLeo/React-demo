0df1ee5a35f583a00dbd053dc508cceeb3501f40
Baoyang Li <lbyang222@gmail.com>
2016-07-04 21:26:33 +0800

    Add files via upload

---
+++ LikeButton/index.js
@@ -0,0 +1,33 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import React, { Component, PropTypes } from 'react';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+var React = require('react');
+var ReactDOM = require('react-dom');
+class LikeButton extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      liked: false
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState({liked: !this.state.liked});
+  }
+  render() {
+    const text = this.state.liked ? 'like' : 'haven\'t li';
+    return (
+      <div onClick={this.handleClick}>
+        You {text} this. Click to toggle.
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <LikeButton />,
+  document.getElementById('app')
+);
+// export default LikeButton;
